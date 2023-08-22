92186c379e5ea8949d43071be3414355c77de119
Daniel Vinicius <viniccius774@gmail.com>
2023-08-22 09:03:37 -0300

    Obtendo usuário por id e tratamento de exceção

--- src/main/java/com/education/workshopspringbootmongodb/services/UserService.java
+++ src/main/java/com/education/workshopspringbootmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.education.workshopspringbootmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.education.workshopspringbootmongodb.domain.User;
 import com.education.workshopspringbootmongodb.repositories.UserRepository;
+import com.education.workshopspringbootmongodb.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> user = repository.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
     }
     
 }
