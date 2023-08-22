b7a188493769caecd0e17226bc31691c59805844
Daniel Vinicius <viniccius774@gmail.com>
2023-08-22 09:19:51 -0300

    Inserção de usuário

--- src/main/java/com/education/workshopspringbootmongodb/services/UserService.java
+++ src/main/java/com/education/workshopspringbootmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.education.workshopspringbootmongodb.domain.User;
+import com.education.workshopspringbootmongodb.dto.UserDTO;
 import com.education.workshopspringbootmongodb.repositories.UserRepository;
 import com.education.workshopspringbootmongodb.services.exceptions.ObjectNotFoundException;
 
@@ -24,5 +25,13 @@ public class UserService {
         Optional<User> user = repository.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
     }
-    
+
+    public User insert(User user) {
+        return repository.insert(user);
+    }
+
+    public User fromDTO(UserDTO userDTO) {
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
+    }
+
 }
