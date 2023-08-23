391485135434fceedfc54aeae09f8a23f719e42b
Daniel Vinicius <viniccius774@gmail.com>
2023-08-23 20:50:47 -0300

    referenciando os posts de um usuário

--- src/main/java/com/education/workshopspringbootmongodb/domain/User.java
+++ src/main/java/com/education/workshopspringbootmongodb/domain/User.java
@@ -1,8 +1,11 @@
 package com.education.workshopspringbootmongodb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "user") // opcional pois o spring busca uma collection com mesmo nome da classe
@@ -14,6 +17,9 @@ public class User implements Serializable {
     private String id;
     private String name;
     private String email;
+
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<Post>();
 
     public User() {
     }
@@ -48,6 +54,10 @@ public class User implements Serializable {
         this.email = email;
     }
 
+    public List<Post> getPosts() {
+        return posts;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -72,7 +82,5 @@ public class User implements Serializable {
             return false;
         return true;
     }
-
-    
 
 }
