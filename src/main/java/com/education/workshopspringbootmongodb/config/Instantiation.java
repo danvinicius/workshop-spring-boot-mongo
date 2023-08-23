ffb6f7b2c4381433b7459f6b6243804552ef0454
Daniel Vinicius <viniccius774@gmail.com>
2023-08-23 20:43:54 -0300

    projeção de dados do autor com DTO

--- src/main/java/com/education/workshopspringbootmongodb/config/Instantiation.java
+++ src/main/java/com/education/workshopspringbootmongodb/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.education.workshopspringbootmongodb.domain.Post;
 import com.education.workshopspringbootmongodb.domain.User;
+import com.education.workshopspringbootmongodb.dto.AuthorDTO;
 import com.education.workshopspringbootmongodb.repositories.PostRepository;
 import com.education.workshopspringbootmongodb.repositories.UserRepository;
 
@@ -33,10 +34,11 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Pariu viagem!", "Partiu viajar para São Paulo", maria);
-        Post post2 = new Post(null, sdf.parse("22/03/2018"), "Bom dia!", "Acordei feliz hoje!", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Pariu viagem!", "Partiu viajar para São Paulo", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("22/03/2018"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 
---
+++ src/main/java/com/education/workshopspringbootmongodb/dto/AuthorDTO.java
@@ -0,0 +1,69 @@
+package com.education.workshopspringbootmongodb.dto;
+
+import java.io.Serializable;
+
+import com.education.workshopspringbootmongodb.domain.User;
+
+public class AuthorDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String name;
+
+    public AuthorDTO(User user) {
+        this.id = user.getId();
+        this.name = user.getName();
+    }
+
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        AuthorDTO other = (AuthorDTO) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
+
+}
