0d4c344cd69acd5c9188ded3a7b8781d00a180a8
Daniel Vinicius <viniccius774@gmail.com>
2023-08-23 21:05:25 -0300

    Obtendo post por id

---
+++ src/main/java/com/education/workshopspringbootmongodb/services/PostService.java
@@ -0,0 +1,23 @@
+package com.education.workshopspringbootmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.education.workshopspringbootmongodb.domain.Post;
+import com.education.workshopspringbootmongodb.repositories.PostRepository;
+import com.education.workshopspringbootmongodb.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repository;
+
+
+    public Post findById(String id) {
+        Optional<Post> post = repository.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
+    }
+}
